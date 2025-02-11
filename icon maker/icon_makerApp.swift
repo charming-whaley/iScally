10a11f7ff00a55c0d89b9cae96a022bea9c14be4
Katkov Fedor <104508411+charming-whaley@users.noreply.github.com>
2025-02-10 17:51:03 -0800

    Initial Commit

---
+++ icon maker/icon_makerApp.swift
@@ -0,0 +1,17 @@
+//
+//  icon_makerApp.swift
+//  icon maker
+//
+//  Created by Fedor Katkov on 10.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct icon_makerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
