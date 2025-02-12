928476d90da4b390b2bea3c82edf922d71738b48
Katkov Fedor <104508411+charming-whaley@users.noreply.github.com>
2025-02-11 20:23:07 -0800

    Updated BackgroundColorSwitcher UI

---
+++ icon maker/Resources/Extensions/Color+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Color+Extensions.swift
+//  icon maker
+//
+//  Created by Fedor Katkov on 11.02.2025.
+//
+
+import Foundation
---
+++ icon maker/Resources/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor+Extensions.swift
+//  icon maker
+//
+//  Created by Fedor Katkov on 11.02.2025.
+//
+
+import Foundation
